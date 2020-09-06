1f694f3865a95a2811bd3e9504cd52787e737810
marcossalas1994 <70782971+marcossalas1994@users.noreply.github.com>
2020-09-06 20:27:24 -0300

    guia 0 ejercicio 4
    0 - 4

---
+++ ProyectoUno/ProyectoUno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ejerciciosCAI
+{//guia 0 ejercicio 4
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text;
+            string textb;
+
+            text = Console.ReadLine();
+            textb = Console.ReadLine();
+            areAnagrams(text, textb);
+
+            void areAnagrams(String first, String second)
+            {
+
+                char[] one = first.ToCharArray();
+                Array.Sort(one);
+                char[] two = second.ToCharArray();
+                Array.Sort(two);
+
+                Console.WriteLine(one.SequenceEqual(two));
+
+            }
+        }
+    }
+}
