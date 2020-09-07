9acb3c04436bdd6e3db3ae45d16cf1a4c3ce9c6d
marcossalas1994 <70782971+marcossalas1994@users.noreply.github.com>
2020-09-06 21:11:12 -0300

    3 y 4

---
+++ ProyectoUno/ejercicio3/ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            Console.WriteLine("Ingresar palabra capicua");
+            palabra=Console.ReadLine();          
+            Console.WriteLine(IsPalindrome(palabra));
+            bool IsPalindrome(string text)
+            {
+                if (text.Length <= 1)
+                    return true;
+                else
+                {
+                    if (text[0] != text[text.Length - 1])
+                        return false;
+                    else
+                        return IsPalindrome(text.Substring(1, text.Length - 2));
+                }
+            }
+        }
+    }
+}
